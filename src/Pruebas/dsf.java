923895c56663fdaff5c118b5e0a47c5ea6b2277b
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2019-03-20 23:28:20 -0600

    Update dsf.java

--- src/Pruebas/dsf.java
+++ src/Pruebas/dsf.java
@@ -1,28 +1,10 @@
 package Pruebas;
 import java.io.*;
 
-public class dsf {
+public class Main_pruebas {
 	public static void main(String [] arg) throws FileNotFoundException {
 		Generador_Diccionario ejem = new Generador_Diccionario();
-		
-		ejem.XD();
-		//String a="hola";
-		//System.out.print(ejem.method(a));
-		//ejem.HL();//se meten el txt a la lista
-		
-		//System.out.print(ejem.Datos.Buscar_dato("hola"));
-		
-		//ejem.listaDiccionario();//meto el txt en la lista
-		
-		//System.out.print(ejem.Datos.tamaño);
-		//Lista<String> xd= new Lista<String>();
-		//xd.addlist("hola");
-		//xd.addlist("je");
-		//System.out.print(xd.Buscar_dato("hola"));
-		
-		//ejem.Datos.addfirst("holla");
-		//ejem.Datos.printL();
-		
+		ejem.Generador_lista_Diccionario();//meto el txt en la lista
 		System.out.print(ejem.Datos.Buscar_dato("mundo"));
 	}
 }
