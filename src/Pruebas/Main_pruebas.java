7824e8dddea6fab2cc427035c7a610de446339e5
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2019-03-29 00:30:09 -0600

    Update Main_pruebas.java

--- src/Pruebas/Main_pruebas.java
+++ src/Pruebas/Main_pruebas.java
@@ -5,6 +5,6 @@ public class Main_pruebas {
 	public static void main(String [] arg) throws FileNotFoundException {
 		Generador_Diccionario ejem = new Generador_Diccionario();
 		ejem.Generador_lista_Diccionario();//meto el txt en la lista
-		System.out.print(ejem.Datos.Buscar_dato("mundo"));
+		System.out.print(ejem.Datos.buscarDato("mundo"));
 	}
 }
